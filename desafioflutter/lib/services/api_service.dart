cd491e0f77de34a4e7c2c616b33c5d96d3ac3de9
newtsarthur <tutuhoficial13@gmail.com>
2025-02-06 20:30:41 -0300

    adição da service para consumir a api de login

---
+++ desafioflutter/lib/services/api_service.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = "http://localhost:8080/auth/login"
+
+  Future<String> login(String email, String senha) async {
+    final response = await http.post(
+      Uri.parse(baseUrl),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"email": email, "senha": senha}),
+    );
+    
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception("Credências inválidas!");
+    }
+  }
+}
