f24f1973d3d7bd02646ec67819c2d9452667c156
newtsarthur <tutuhoficial13@gmail.com>
2025-02-06 03:22:14 -0300

    form de login

--- desafioflutter/lib/login.dart
+++ desafioflutter/lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,20 +7,38 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Location Changer")),
-      body: Padding(
+      // appBar: AppBar(title: const Text("Location Changer")),
+      body: Container(
+        color: CupertinoColors.destructiveRed,
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            const Text("Location Changer",
+            style: TextStyle(
+              fontSize: 24,
+              height: 3,
+              color: CupertinoColors.lightBackgroundGray,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,),
+            const Text("Plugin for Tinder",
+            style: TextStyle(
+              fontSize: 15,
+              height: 3,
+              color: CupertinoColors.lightBackgroundGray,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+            ),
             TextField(
               decoration: const InputDecoration(
                 labelText: "Email",
                 border: OutlineInputBorder(),
               ),
             ),
-            const SizedBox(height: 10),
+            const SizedBox(height: 20),
             TextField(
               obscureText: true,
               decoration: const InputDecoration(
@@ -27,15 +46,20 @@ class LoginPage extends StatelessWidget {
                 border: OutlineInputBorder(),
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 24),
             ElevatedButton(
               onPressed: () {
 
               },
               style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(vertical: 12),
-
-              ), child: const Text("Login"),
+                padding: const EdgeInsets.symmetric(vertical: 25),
+              ), child: const Text("Login",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: CupertinoColors.destructiveRed,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
           ],
         ),
